b03d2cd05875d49deaf866d646bc37a5a4f1ad00
Nadir Djebbar <nadirdjebbar@gmail.com>
2020-10-22 20:02:51 +0200

    update quizz

--- src/components/QuizzSystemeSco.jsx
+++ src/components/QuizzSystemeSco.jsx
@@ -16,11 +16,18 @@ state={
 		countGoodAnswers: 0,
 		countQuestions: 0,
 		numberQuestion: 1,
-		gradeAppears: null
+		gradeAppears: null,
+		correctAnswer: ""
 	}
 
 	componentDidMount = () => {
 		this.questionGet()
+	}
+
+	restartQuizz = () => {
+
+		//document.location.reload(true);
+		window.location.reload()
 	}
 
 
@@ -42,12 +49,153 @@ state={
 
 								}, 3000)
 
-		}
-
-		if(this.state.countQuestions === 3 && this.state.countGoodAnswers === 1){
+		}if(this.state.countQuestions === 8 && this.state.countGoodAnswers <= 1){
 			if(this.state.gradeAppears == null )
 
-				this.setState({gradeAppears: <img src="./images/Samurai.jpg" className ="samuraï" />})
+			/*setTimeout(() => {this.setState({gradeAppears: <img src="./images/Samurai4.jpg" className ="samurai" />})
+
+			 }, 3000)*/
+
+			
+			setTimeout(() => {this.setState({gradeAppears: 
+			<div style={{position: "relative", height:"auto"}}>
+   				<div style={{position: "absolute", zIndex:"1", height: "auto", width: "100%"}}>	
+					<img src="./images/samouraiVaincu.jpg" style={{width: "100%"}}/>
+				</div>
+				
+			   <div style={{position: "absolute", width: "100%", height:"auto", zIndex:"2", fontSize: "2rem", color: "aliceblue"}}>
+			      <center><b>GRADE: Guerrier Vaincu !<br /> {this.state.countGoodAnswers} bonne réponse</b>
+			      <br />
+			      	   <p>
+			     		 Les défaites d'aujourd'hui construisent les victoires de demain !
+			     		 <br />
+
+			           </p>
+			           <button onClick={this.restartQuizz}>RESTART</button>
+			      </center>
+			    </div>
+			</div>     	
+			}) 
+			}, 3000)
+		}
+
+		if(this.state.countQuestions === 8 && this.state.countGoodAnswers >= 2 && this.state.countGoodAnswers < 5){
+			if(this.state.gradeAppears == null )
+
+			/*setTimeout(() => {this.setState({gradeAppears: <img src="./images/Samurai4.jpg" className ="samurai" />})
+
+			 }, 3000)*/
+
+			
+			setTimeout(() => {this.setState({gradeAppears: 
+			<div style={{position: "relative", height:"auto"}}>
+   				<div style={{position: "absolute", zIndex:"1", height: "auto", width: "100%"}}>	
+					<img src="./images/Samurai4.jpg" style={{width: "100%"}}/>
+				</div>
+				
+			   <div style={{position: "absolute", width: "100%", height:"auto", zIndex:"2", fontSize: "2rem", color: "aliceblue"}}>
+			      <center><b>GRADE: Samouraï !<br /> {this.state.countGoodAnswers} bonnes réponses</b>
+			      <br />
+			      	   <p>
+			     		 La sagesse mets du temps à être acquise mais tu es sur la bonne voie !
+			     		 <br />
+
+			           </p>
+			           <button onClick={this.restartQuizz}>RESTART</button>
+			      </center>
+			    </div>
+			</div>     	
+			}) 
+			}, 3000)
+		}
+
+		
+
+		if(this.state.countQuestions === 8 && this.state.countGoodAnswers >=5 && this.state.countGoodAnswers < 7){
+			if(this.state.gradeAppears == null )
+
+			/*setTimeout(() => {this.setState({gradeAppears: <img src="./images/Samurai4.jpg" className ="samurai" />})
+
+			 }, 3000)*/
+
+			
+			setTimeout(() => {this.setState({gradeAppears: 
+			<div style={{position: "relative", height:"auto"}}>
+   				<div style={{position: "absolute", zIndex:"1", height: "auto", width: "100%"}}>	
+					<img src="./images/Samurai4.jpg" style={{width: "100%"}}/>
+				</div>
+				
+			   <div style={{position: "absolute", width: "100%", height:"auto", zIndex:"2", fontSize: "2rem", color: "aliceblue"}}>
+			      <center><b>GRADE: Samouraï !<br /> {this.state.countGoodAnswers} bonnes réponses</b>
+			      <br />
+			      	   <p>
+			     		 La sagesse mets du temps à être acquise mais tu es sur la bonne voie !
+			     		 <br />
+
+			           </p>
+			           <button onClick={this.restartQuizz}>RESTART</button>
+			      </center>
+			    </div>
+			</div>     	
+			}) 
+			}, 3000)
+		}
+
+		else if(this.state.countQuestions === 8 && this.state.countGoodAnswers >=7){
+			if(this.state.gradeAppears == null )
+
+			
+			setTimeout(() => {this.setState({gradeAppears: 
+			<div style={{position: "relative", height:"auto"}}>
+   				<div style={{position: "absolute", zIndex:"1", height: "auto", width: "100%"}}>	
+					<img src="./images/shogun.jpg" style={{width: "100%"}}/>
+				</div>
+				
+			   <div style={{position: "absolute", width: "100%", height:"auto", zIndex:"2", fontSize: "2rem", color: "whitesmoke"}}>
+			      <center><b>GRADE: Shogun !<br /> {this.state.countGoodAnswers} bonnes réponses</b>
+			      <br />
+			      	   <p>
+			     		 Tu as l'âme d'un leader !
+			     		 <br />
+
+			           </p>
+			           <button onClick={this.restartQuizz}>RESTART</button>
+			      </center>
+			    </div>
+			</div>     	
+			}) 
+			}, 3000)
+		}
+
+		else if(this.state.countQuestions === 8 && this.state.countGoodAnswers >= 6){
+			if(this.state.gradeAppears == null )
+
+			
+			setTimeout(() => {this.setState({gradeAppears: 
+			<div style={{position: "relative", height:"auto"}}>
+   				<div style={{position: "absolute", zIndex:"1", height: "auto", width: "100%"}}>	
+					<img src="./images/legend.jpg" style={{width: "100%"}}/>
+				</div>
+				
+			   <div style={{position: "absolute", width: "100%", height:"auto", zIndex:"2", fontSize: "2rem", color: "whitesmoke"}}>
+			      <center><b>GRADE: Soldat de Légende !<br /> {this.state.countGoodAnswers} bonnes réponses</b>
+			      <br />
+			      	   <p>
+			     		 Véritable connaisseur !
+			     		 <br />
+
+			           </p>
+			           <button onClick={this.restartQuizz}>RESTART</button>
+			      </center>
+			    </div>
+			</div>     	
+			}) 
+			}, 3000)
+		}
+
+		if(this.state.gradeAppears !== null){
+			const div = document.getElementById("conteneurQuizzSystemeSco");
+			div.style.display="none"
 		}
 		
 	}
@@ -59,6 +207,12 @@ state={
 			console.log("two")
 		}
 
+	}*/
+
+	/*restartQuizz = () => {
+
+		//document.location.reload(true);
+		window.location.reload()
 	}*/
 
 	handleClick = (e) =>  {
@@ -116,9 +270,8 @@ state={
 		.then(res => {
 			if (res.status === 200) {
 				res.json().then(res => {
-					console.log("Réponse postée")
-					console.log(res)
-					//this.state.goodAnswers = res.;
+					console.log("Réponse postée")					
+					//this.setState({correctAnswer: res.Answer1})
 					
 
 					if(res == true){
@@ -128,7 +281,8 @@ state={
 
 					else {
 						this.setState({falseAnswers: true})
-						
+						this.setState({correctAnswer: res})
+						console.log(res, "229")
 					}
 					
 				})
@@ -205,11 +359,11 @@ state={
 		const countQuestions = this.state.countQuestions
 		
 		//console.log(div)
-		if (countQuestions !== 3){
+		if (countQuestions !== 8){
 		this.setState({countQuestions: this.state.countQuestions + 1})
 		}
 
-		if (index < 2) {
+		if (index < 7) {
 		this.setState({index: this.state.index + 1})
 			//console.log(index)
 		}
@@ -224,11 +378,13 @@ state={
 
 		const actualNumber = this.state.numberQuestion;
 
-		if (actualNumber < 3) {
-		this.setState({numberQuestion: this.state.numberQuestion + 1})
-			//console.log(index)
+		if (actualNumber < 8) {
+		setTimeout(()=> {	
+			this.setState({numberQuestion: this.state.numberQuestion + 1})
+				//console.log(index)
+			
+			}, 3000)	
 		}
-
 	}
 
 	render() {
@@ -255,16 +411,20 @@ state={
 					 
 				)); 
 
-					
+					const badResult = (<div className="falseAnswers"><h2>Faux ! La bonne réponse est :"{this.state.correctAnswer}"</h2></div>)
+
+					const result = (<div className="goodAnswers"><h2>Bonne réponse !</h2></div>)
 
     				const returnQuizz = (
 
 							<div className="test">
-							<h2>{this.state.numberQuestion}</h2>
+							{this.state.goodAnswers === true ? result : null}
+							{(this.state.falseAnswers === true) ? badResult : null}
+							<p className="numberQuestion">{this.state.numberQuestion}</p>
 							  {mapQuestion[this.state.index]}
 								  <form className="conteneurBack" onSubmit={this.handleClick}>
 									<input type="text" name="reponse" value ={this.state.reponse} 
-									onChange={this.handleChange} className="title"/><br />
+									onChange={this.handleChange} className="title" required /><br />
 									<button className="buttonSignup">NEXT</button>
 								</form> 
 							</div> 
@@ -275,22 +435,26 @@ state={
 					
 
 
-					const result = (<div className="goodAnswers"><h2>Bonne réponse !</h2></div>)
-					const badResult = (<div className="falseAnswers"><h2>Mauvaise réponse !</h2></div>)
+					
+					
 					console.log(this.state.countQuestions, "countQuestions")
 					console.log(this.state.countGoodAnswers, "CountGoodAnswers")
 					
 				
 		return(
-			<div className="conteneurQuizzSystemeSco">
-							
-						  {returnQuizz}
-						  {this.state.goodAnswers === true ? result : null}
-						  {(this.state.falseAnswers === true) ? badResult : null}	
-						  {this.state.gradeAppears !== null ? this.state.gradeAppears : null} 
+
+
+		<div className="bigConteneur">	
+		 {this.state.gradeAppears !== null ? this.state.gradeAppears : null}
+			<div className="conteneurQuizzSystemeSco" id="conteneurQuizzSystemeSco">
+				
+						  {returnQuizz}		  
+					<p></p>	  					   
 						  							 
 				 
 			</div>
+
+		</div>	
 
 		);
 
